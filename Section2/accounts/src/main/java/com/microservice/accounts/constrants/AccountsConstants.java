a9d0569ce08b3775840b8a5f71194eebf57b58ea
Tahmid Hassan Ridoy <tahmidhassanhridoy@gmail.com>
2026-04-13 11:36:48 +0600

    new codes

--- Section2/accounts/src/main/java/com/microservice/accounts/constrants/AccountsConstants.java
+++ Section2/accounts/src/main/java/com/microservice/accounts/constrants/AccountsConstants.java
@@ -9,6 +9,9 @@ public class AccountsConstants {
     public static final String MESSSAGE_201 = "Account created Successfully";
     public static final String STATUS_200 = "200";
     public static final String MESSAGE_200 = "Request processed successfully";
+    public static final String STATUS_417 = "417";
+    public static final String MESSAGE_417_UPDATE = "Update Operation Failed. PLease Try again or contact Dev Team";
+    public static final String MESSAGE_417_DELETE = "Delete Operation Failed. PLease Try again or contact Dev Team";
     public static final String STATUS_500 = "500";
     public static final String MESSAGE_500 = "An error occurred. Please try again or contact Dev team";
 
--- Section2/accounts/src/main/java/com/microservice/accounts/dto/AccountsDto.java
+++ Section2/accounts/src/main/java/com/microservice/accounts/dto/AccountsDto.java
@@ -1,19 +1,33 @@
 package com.microservice.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Schema(
+        name = "Accounts",
+        description = "Schema to hold Account information"
+)
 public class AccountsDto {
 
     @NotEmpty(message = "AccountNumber Can not be null or Empty")
     @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
+    @Schema(
+            description = "Account Number of Bank", example = "1659843347"
+    )
     private Long accountNumber;
 
     @NotEmpty(message = "AccountType Number Can not be null or Empty")
+    @Schema(
+            description = "Account type of Ban Account", example = "Savings"
+    )
     private String accountType;
 
     @NotEmpty(message = "BrunchAddress Number Can not be null or Empty")
+    @Schema(
+            description = "Bank Account Address "
+    )
     private String brunchAddress;
 }
